b1d829fd316824c0538e24a2f0cb581d96c796f3
Rebecca Lain <rebeccalain@Rebeccas-MacBook-Air.local>
2022-05-02 18:53:31 +0200

    Added skycons.js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # weather-app
-2022 Fun Project: Building a Weather App
+### 2022 Fun Project: Building a Weather App
+*Built*: April 2022
+*Tutorial*: https://www.youtube.com/watch?v=wPElVpR1rwA&ab_channel=DevEd
+*OpenSource* Weather API: https://openweathermap.org/current
+*Icon Images*: https://darkskyapp.github.io/skycons/
+
--- code/app.js
+++ code/app.js
@@ -18,6 +18,10 @@ timestamp: 1651299453041
 window.addEventListener('load', () => {
   let long;
   let lat;
+  let timezone;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degree'); 
+  let locationTimezone = document.querySelector('.location-timezone'); 
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -26,27 +30,49 @@ window.addEventListener('load', () => {
         console.log(position);
         long = position.coords.longitude;
         lat = position.coords.latitude;
-        //const api = ``;
-        console.log("About to fetch")
-       //fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`, {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=d97ccee936a02298064e1d8ae3bd38f8`, {
-        //   method: 'GET',
-        //   //body: JSON.stringify(data),
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //     Authorization: `Basic ${Buffer.from(API_KEY).toString('base64')}`,
-        //   },
-        })
-          .then(response => {
-              return response.json();
+        //fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`, {
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=d97ccee936a02298064e1d8ae3bd38f8`,
+          {
+            method: 'GET',
+          },
+        )
+          .then((response) => {
+            return response.json();
           })
-          .then(json => {
-          console.log(json);
-        });
-      });
-  }
-     else {
-    //throw new Error("Oh no! We can't get access to your location :(")
+          .then((json) => {
+            console.log(json);
+            const {description, icon, main} = json.weather[0];
+            //Set DOM Elements from the API
+            temperatureDegree.textContent = json.main.feels_like;
+            temperatureDescription.textContent = main;
+            locationTimezone.textContent = json.timezone;
+          });
+      },
+    );
+  } else {
     h1.textContent("Oh no! We can't get access to your location :(");
   }
 });
+
+
+// function setIcons(icon, iconID){
+//     const skycons = new Skycons({color: "white"});
+//     const currentIcon = (icon) => {
+//         switch(icon){
+//             case '01d'      : return 'CLEAR_DAY';
+//             case '01n'      : return 'CLEAR_NIGHT';
+//             case '02d'      : return 'PARTLY_CLOUDY_DAY';
+//             case '02n'      : return 'PARTLY_CLOUDY_NIGHT';
+//             case '03n || 03d || 04d || 04n'      : return 'CLOUDY';
+//             case '10d || 10n || 09d || 09n'      : return 'RAIN';
+//             case '11d || 11n'      : return 'SLEET';
+//             case '13d || 13n'      : return 'SNOW';
+//             case '50d || 50n'      : return 'WIND';
+//             case '50d || 50n'      : return 'FOG';
+//             default        : return 1;
+//         }
+//     };  
+//     skycons.play();
+//     return skycons.set(iconID, Skyecons[currentIcon]);
+// }
