6f895c62c5343afdf885317acf3212aa39416e15
Rebecca Lain <rebeccalain@Rebeccas-MacBook-Air.local>
2022-05-02 21:21:07 +0200

    Added comments and cleaned up codebase

--- code/01-app.js
+++ code/01-app.js
@@ -1,21 +1,8 @@
-//import "./env.js";
 
-/* navigator.geolocation.getCurrentPosition access the following:
-GeolocationPosition {coords: GeolocationCoordinates, timestamp: 1651299453041}
-coords: GeolocationCoordinates
-accuracy: 19.098
-altitude: null
-altitudeAccuracy: null
-heading: null
-latitude: -34.0772854
-longitude: 18.4572942
-speed: null
-[[Prototype]]: GeolocationCoordinates
-timestamp: 1651299453041
-[[Prototype]]: GeolocationPosition
-*/
-
-
+/**
+ * Function to complete a GET request from the Open Weather API based on the user's location
+ * to then return related Weather information.
+ */
 
 window.addEventListener('load', () => {
   let long;
@@ -33,7 +20,6 @@ window.addEventListener('load', () => {
         console.log(position);
         long = position.coords.longitude;
         lat = position.coords.latitude;
-        //fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`, {
         fetch(
           `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=d97ccee936a02298064e1d8ae3bd38f8`,
           {
@@ -51,10 +37,7 @@ window.addEventListener('load', () => {
             temperatureDescription.textContent = main;
             locationTimezone.textContent = json.timezone;
             //Set icon
-            weatherIcon.innerHTML = setIcons(icon);
-      
-
-            
+            weatherIcon.innerHTML = setIcons(icon);      
           });
       },
     );
